45852f69f82817709e288033395797201a3c9be1
Marco Schiavone <marko.skiaa40@gmail.com>
2024-08-09 18:27:34 +0200

    Update ServerController.java
    still not working... idk why

--- src/main/java/project/server/ServerController.java
+++ src/main/java/project/server/ServerController.java
@@ -1,35 +1,28 @@
 package project.server;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
 
 public class ServerController {
     @FXML
-    private Label welcomeText;
-    private final ServerSocket serverSocket;
-    private final ExecutorService pool;
+    private ListView<String> logList;
+    private ServerSocket serverSocket;
+    private ExecutorService pool;
 
-    /*
-     *  @param poolSize: how many clients it is possible to manage in the same time, it is provided from the server app
-     * 
-     */
+    private final ServerModel model;
 
-   public ServerController(int port, int poolSize) throws IOException 
-   {
-     serverSocket = new ServerSocket(port);
-     pool = Executors.newFixedThreadPool(poolSize);
-   }
-
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Server!");
+    public ServerController() {
+        try {
+            serverSocket = new ServerSocket(ServerModel.getPORT());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            model = new ServerModel();
+        }
     }
 
     /*
@@ -39,37 +32,43 @@ public class ServerController {
      *          -Socket : to create the real connection between a "server-thread" and the client
      *          -ExecutorService: to create threads with the goal to manage different connections for the almost certain possibility
      *                             to communicate with multiple clients at the same time
-     *          
      */
     public void serverStart()
     {
+        try {
+
+            pool = Executors.newFixedThreadPool(10);
+            System.out.println("Server is up at port " + serverSocket.getLocalPort() + ".\n");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
         try
         {
-            System.out.println("Server is up at port "+ serverSocket.getLocalPort() + "!!\n");
-
-
-            while (!Thread.currentThread().isInterrupted()) 
+            while (!Thread.currentThread().isInterrupted())
             {
                 Socket clientSocket = serverSocket.accept();
-                pool.execute(new ThreadServer(serverSocket.accept()));
+                pool.execute(new ThreadServer(clientSocket));
             }
         }
-        catch(IOException e)
+        catch(Exception e)
         {
             e.printStackTrace();
         }
     }
 
-    public void ServerStop()
+    /** Function called by the {@link ServerApplication} when closing event is fired for the server.
+     * @note It ENSURES all server services being properly closed before exiting the program.
+     * */
+    public void serverStop()
     {
         try {
             if(!pool.isShutdown())
                 pool.shutdown();
             if(!serverSocket.isClosed())
                 serverSocket.close();
-
         } catch (Exception e) {
-            System.out.println("not shut down correctly");
+            System.out.println("Error during server shut down.");
             e.printStackTrace();
         }
     }
