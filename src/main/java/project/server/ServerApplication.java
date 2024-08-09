2b415f16202a131cbfc7f72f3859beb6ad65cd38
Marco Schiavone <marko.skiaa40@gmail.com>
2024-08-09 18:28:06 +0200

    Update ServerApplication.java

--- src/main/java/project/server/ServerApplication.java
+++ src/main/java/project/server/ServerApplication.java
@@ -5,21 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 
 public class ServerApplication extends Application {
     private String userEmail;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ServerApplication.class.getResource("log-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        ServerController controller = new ServerController(69420, 0);
-        stage.setTitle("Server");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.sizeToScene();
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(ServerApplication.class.getResource("log-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 900, 500);
+            ServerController serverController = fxmlLoader.getController();
+            stage.setTitle("Server");
+            stage.setScene(scene);
+            stage.setResizable(false);
+
+            // Setting function for closing operation
+            stage.setOnCloseRequest(event -> serverController.serverStop());
+            stage.sizeToScene();
+            stage.show();
+
+            // Starting the server
+            serverController.serverStart();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
