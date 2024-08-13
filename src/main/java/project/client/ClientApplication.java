a6d1613546bbc82aa9c65e48ad892ff2ba8b857f
Marco Schiavone <marko.skiaa40@gmail.com>
2024-08-13 11:55:24 +0200

    Starting to create mail-view

--- src/main/java/project/client/ClientApplication.java
+++ src/main/java/project/client/ClientApplication.java
@@ -9,8 +9,10 @@ public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("login-view.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+            //FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("login-view.fxml"));
+            //Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+            FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("mail-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
             stage.setTitle("Client - LogIn");
             stage.setScene(scene);
             stage.setResizable(false);
--- src/main/java/project/client/MailController.java
+++ src/main/java/project/client/MailController.java
@@ -1,5 +1,26 @@
 package project.client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 /** Controller used to bind "model" and "view" for a single mail. */
 public class MailController {
+
+    @FXML
+    private TextField subjectText;
+
+    @FXML
+    private Button sendBtn;
+
+
+    @FXML
+    protected void sendMail() {
+        if (checkFields()) {
+            // @todo finish it
+        }
+    }
+
+    private boolean checkFields() {
+        return (subjectText.getText() == null || subjectText.getText().isBlank()); // @todo finish this
+    }
 }
