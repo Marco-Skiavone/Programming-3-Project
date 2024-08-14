30272f3e1a1d106c80b9ab80de73b08a96a8efdb
Marco Schiavone <marko.skiaa40@gmail.com>
2024-08-14 11:51:49 +0200

    Mail-view (still WIP)

--- src/main/java/project/client/MailController.java
+++ src/main/java/project/client/MailController.java
@@ -2,25 +2,125 @@ package project.client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+import project.utilities.*;
 
 /** Controller used to bind "model" and "view" for a single mail. */
 public class MailController {
-
     @FXML
-    private TextField subjectText;
-
+    private Label sender;
+    @FXML
+    private TextField subjectField;
+    @FXML
+    private TextField receiversField;
+    @FXML
+    private TextArea mailText;
+    @FXML
+    private Label errorText;    // Label that pops up a red message if some fields are wrong.
+    @FXML
+    private Button replyBtn;
+    @FXML
+    private Button replyAllBtn;
+    @FXML
+    private Button forwardBtn;
     @FXML
     private Button sendBtn;
 
+    private MailModel model;
+    private String user;    // The client address linked to this controller-view
 
+    /** Function called by FXML to bind the ListView to the logMsgList */
     @FXML
-    protected void sendMail() {
-        if (checkFields()) {
-            // @todo finish it
+    public void initialize() {
+
+    }
+
+    /** Function that sets the response button to "disabled".
+     * @Note: The 3 response buttons are:<br>
+     * - {@link #forwardBtn}<br>
+     * - {@link #replyBtn}<br>
+     * - {@link #replyAllBtn} */
+    private void setDisableResponseButton(boolean disabled) {
+        forwardBtn.setDisable(disabled);
+        replyBtn.setDisable(disabled);
+        replyAllBtn.setDisable(disabled);
+    }
+
+    public void setUpForward (MailModel model, String userAddress, Email email) {
+
+    }
+
+    public void setUpReply (MailModel model, String userAddress, Email email, boolean replyAll) {
+
+    }
+
+    public void setUpNewMail (MailModel model, String userAddress) {
+        try {
+            this.user = userAddress;    // @todo check
+            sender.setText(user);
+            /* We remove the common part of the title and extract the type of editing we want to do. */
+            String type = ((Stage) sender.getScene().getWindow()).getTitle().split(" ")[2];
+            if (!type.equals("View") && user != null)
+                sender.setText(user);
+            switch (type) {
+                case "View":
+                    // Can NOT modify anything, but can trigger "response" buttons
+                    subjectField.setDisable(true);
+                    receiversField.setDisable(true);
+                    mailText.setDisable(true);
+                    sendBtn.setDisable(true);
+                    break;
+                case "Forward":
+                    // can modify Receivers
+                    sendBtn.setDisable(true);
+                    subjectField.setDisable(true);
+                    mailText.setDisable(true);
+                    setDisableResponseButton(true);
+                    break;
+                case "Reply":
+                case "Reply All":
+                    // can modify Text (and trigger Send)
+                    subjectField.setDisable(true);
+                    receiversField.setDisable(true);
+                    setDisableResponseButton(true);
+                    break;
+                case "New":
+                    // can modify everything, but the "response" buttons
+                    setDisableResponseButton(true);
+                    break;
+                default:
+                    throw new RuntimeException("Unknown Email Type.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.model = model;
         }
     }
 
+    /** Function called when the "send" button is pressed in the "mail-view".
+     * It checks all the fields calling {@link #checkFields}, then it calls the model function to FETCH a new Email. */
+    @FXML
+    protected void sendMail() {
+        if (!checkFields())
+            errorText.setText("Invalid Arguments");
+        else {
+            // @todo finish it with a model method call
+        }
+    }
+
+    /** Function that checks the input fields of an email.
+     * @return 'true' - If the fields are filled and the receivers are correct.
+     * (it also contacts the server through the model) */
     private boolean checkFields() {
-        return (subjectText.getText() == null || subjectText.getText().isBlank()); // @todo finish this
+        boolean condition = !subjectField.getText().isBlank() && !receiversField.getText().isBlank() &&
+        !mailText.getText().isBlank();
+        if (!condition) return false;
+        for (String field : receiversField.getText().split(",")) {
+            String adr = field.trim();
+            if (!LoginController.checkSyntax(adr.trim()) || !this.model.checkAddress(adr))
+                return false;
+        }
+        return true;
     }
 }
--- src/main/java/project/client/MailModel.java
+++ src/main/java/project/client/MailModel.java
@@ -1,5 +1,27 @@
 package project.client;
 
+import project.utilities.*;
+
+import java.io.*;
+import java.net.*;
+
 /** Model used to define methods for a single mail visualization/editing. */
 public class MailModel {
+
+    /** Function that asks the server if the string passed is a valid address or not.
+     * @param adr The address to make the server check.
+     * @return a boolean in base of the response from the server. */
+    public boolean checkAddress(String adr) {
+        try (Socket clientSocket = new Socket("127.0.0.1", project.server.ServerModel.getPORT())) {
+            if ( adr == null || adr.isBlank())
+                throw new RuntimeException("Subject field is empty");
+            ObjectOutputStream output = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(clientSocket.getInputStream());
+            output.writeObject("CHECK_ADDR.-/" + adr);  // @todo refactor this line
+            output.flush();
+            return input.readBoolean();
+        } catch(Exception e) {
+            return false;
+        }
+    }
 }
