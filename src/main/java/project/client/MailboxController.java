960895bb11701db9569731f349b1b6f93817a1c3
Marco Schiavone <marko.skiaa40@gmail.com>
2024-08-14 11:51:22 +0200

    Updating Mailbox part

--- src/main/java/project/client/MailboxController.java
+++ src/main/java/project/client/MailboxController.java
@@ -1,4 +1,15 @@
 package project.client;
 
 public class MailboxController {
+    /* The listView here will use a complex class to have:
+     * - a CheckBox,
+     * - a String (to show),
+     * - an ID associated (hidden) */
+
+
+    private final MailboxModel model;
+
+    public MailboxController() {
+        this.model = new MailboxModel();
+    }
 }
