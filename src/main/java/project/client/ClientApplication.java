78911ff5d397fc4a166e563fd6722b5cfdece3a9
Marco Schiavone <marko.skiaa40@gmail.com>
2024-08-14 11:50:45 +0200

    Using ClientApplication for debugging another view

--- src/main/java/project/client/ClientApplication.java
+++ src/main/java/project/client/ClientApplication.java
@@ -9,12 +9,17 @@ public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            //FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("login-view.fxml"));
-            //Scene scene = new Scene(fxmlLoader.load(), 400, 300);
-            FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("mail-view.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-            stage.setTitle("Client - LogIn");
+            FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("login-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 400, 300);
             stage.setScene(scene);
+            /*
+            FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("mail-view.fxml"));  // DEB
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400); // DEB
+            stage.setTitle("Mail - New Mail");  // DEB
+            MailController controller = fxmlLoader.getController();   // DEB
+            controller.setUpNewMail(new MailModel(), "riccardo.saccu@prog3.edu");     // DEB
+            */
+            stage.setTitle("Client - LogIn");
             stage.setResizable(false);
             stage.sizeToScene();
             stage.show();
